5b7da8cfa09f9170c1186777d07718515f90fbcb
Sriram Kashyap <kssriramkashyap002@gmail.com>
2024-01-08 21:49:53 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [goodCount, setGoodCount] = useState(0);
+  const [neutralCount, setNeutralCount] = useState(0);
+  const [badCount, setBadCount] = useState(0);
+
+
+  const giveFeedback = (type) => {
+    switch (type) {
+      case 'good':
+        setGoodCount(goodCount + 1);
+        break;
+      case 'neutral':
+        setNeutralCount(neutralCount + 1);
+        break;
+      case 'bad':
+        setBadCount(badCount + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
+    <div className="main">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Give Feedback</h1>
+      <button onClick={() => giveFeedback('good')}>Good</button>
+      <button onClick={() => giveFeedback('neutral')}>Neutral</button>
+      <button onClick={() => giveFeedback('bad')}>Bad</button>
+
+      <h1>Statistics</h1>
+      <div>
+        <p>Good: {goodCount}</p>
+        <p>Neutral: {neutralCount}</p>
+        <p>Bad: {badCount}</p>
+      </div>
+    </div>
     </div>
   );
 }
